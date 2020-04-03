fefa6b448c185bde81f88a1bf6787d3386a0303f
Jett Crowson <jecr4062@colorado.edu>
2020-04-03 16:47:12 -0600

    Added page for failed login

--- routes/login.js
+++ routes/login.js
@@ -7,17 +7,23 @@ router.post('/', function(req, res, next) {
     var query = `select * from p_user where username = '${req.body.username}' and pwd = '${req.body.password}'`;
 	  db.any(query)
         .then(function (p) {
-            req.session.user = p[0];
-            res.render('index', {
-                title: 'Picopy',
-                user:req.session.user
-              });
+            if(p.length == 0){
+                res.render('cnf', {
+                    title: 'Login Error',
+                  });
+            }else{
+                req.session.user = p[0];
+                res.render('index', {
+                    title: 'Picopy',
+                    user:req.session.user
+                });
+            }
         })
         .catch(function (err) {
             // display error message in case an error
             request.flash('error', err);
             console.log(err);
-            alert("Username/password combo not found. Try again.");
+            
         })
 
 
