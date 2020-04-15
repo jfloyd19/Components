88019241ac71955a1eedb22304fcfa7ade0dedf1
Jett Crowson <jettcrowson@gmail.com>
2020-04-15 16:06:57 -0600

    Change db query to include username

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('../pgp.js');
 
 router.get('/', function(req, res, next) {
-    var query = 'select * from photo;';
+    var query = 'select photo.photo_id, photo.filters, photo.filename, photo.private, photo.date_created, p_user.username from photo inner join p_user on photo.user_id = p_user.user_id;';
     var query2 = 'select count(*) from photo;';
 	  db.task('get-everything', task => {
         return task.batch([
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
             ]);
         })
         .then(function (p) {
+            console.log(p[0][0]);
             res.render('gallery',{
             my_title: "Gallery",
             photo_data: p[0],
